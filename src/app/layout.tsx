e1776fd58a15948ecb774add72ca6b8351485e3e
ahn-sujin <asj9674@gmail.com>
2025-02-17 22:45:07 +0900

    add:메타태그 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import "./globals.css";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Kanban Todo List | FE_안수진",
+  description: "업무의 효율을 증진시키기 위한 칸반형태의 To-Do List",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
